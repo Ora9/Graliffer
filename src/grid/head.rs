d0b630a0a70f1ff5a785517c382d7d8030e326fa
Ora9 <ora9@duck.com>
2025-03-11 12:35:48 +0100

    Use Position::increment() and so for head movements + better doc and tests

--- src/grid/head.rs
+++ src/grid/head.rs
@@ -3,7 +3,7 @@ use anyhow::Context;
 use crate::grid::Position;
 
 /// A [`Head`] always has a direction
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Direction {
     Up,
     Right,
@@ -17,9 +17,26 @@ impl Default for Direction {
     }
 }
 
-/// An [`Head`] has :
-/// - A position
-/// - A direction
+/// An head travels in a [`Grid`] reading [`Operand`] and [`Opcodes`]
+///
+/// an `Head` has :
+/// - A [`Position`] in a [`Grid`]
+/// - A [`Direction`],
+///
+/// # Example
+/// ```
+/// # use graliffer::grid::{Head, Direction, Position};
+/// let pos1 = Position::from_numeric(25, 25).unwrap();
+/// let pos2 = Position::from_numeric(26, 24).unwrap();
+/// let direction = Direction::Down;
+///
+/// let mut head = Head::new(pos1, Direction::Right);
+///
+/// head.take_step();
+/// head.direct_to(Direction::Up);
+/// head.take_step();
+/// assert_eq!(head.position, pos2);
+/// ```
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Head {
     pub position: Position,
@@ -27,6 +44,7 @@ pub struct Head {
 }
 
 impl Head {
+    /// Return an `Head` given a [`Position`] and a [`Direction`]
     pub fn new(position: Position, direction: Direction) -> Self {
         Self {
             position,
@@ -34,26 +52,66 @@ impl Head {
         }
     }
 
-    pub fn move_to(mut self, position: Position) {
+    /// Jump to another [`Position`], without changing the [`Direction`]
+    ///
+    /// # Examples
+    /// ```
+    /// # use graliffer::grid::{Head, Direction, Position};
+    /// let mut head = Head::default();
+    /// assert_eq!(head.position.as_numeric(), (0, 0));
+    ///
+    /// let pos = Position::from_numeric(25, 25).unwrap();
+    /// head.move_to(pos);
+    /// assert_eq!(head.position, pos);
+    /// ```
+    pub fn move_to(&mut self, position: Position) {
         self.position = position;
     }
 
-    pub fn direct_to(mut self, direction: Direction) {
+    /// Change the [`Direction`], without changing the [`Position`], next "step", will be in that set direction
+    ///
+    /// # Examples
+    /// ```
+    /// # use graliffer::grid::{Head, Direction};
+    /// let mut head = Head::default();
+    /// assert_eq!(head.direction, Direction::Right);
+    ///
+    /// head.direct_to(Direction::Up);
+    /// assert_eq!(head.direction, Direction::Up);
+    /// ```
+    pub fn direct_to(&mut self, direction: Direction) {
         self.direction = direction;
     }
 
+    /// Take one step in the [`Head`]'s [`Direction`]
+    ///
+    /// # Errors
+    /// Returns an error if [`Head`] could not step further in that direction,
+    /// because it could not go outside of the [`Grid`]'s limits
+    ///
+    /// # Examples
+    /// ```
+    /// # use graliffer::grid::{Head, Direction, Position};
+    /// let pos = Position::from_numeric(25, 25).unwrap();
+    /// let mut head = Head::new(pos, Direction::Right);
+    ///
+    /// head.take_step();
+    /// head.direct_to(Direction::Down);
+    /// head.take_step();
+    /// head.direct_to(Direction::Left);
+    /// head.take_step();
+    /// head.direct_to(Direction::Up);
+    /// head.take_step();
+    /// assert_eq!(head.position, pos);
+    /// ```
     pub fn take_step(&mut self) -> Result<(), anyhow::Error> {
         use self::Direction::*;
-        let (x_offset, y_offset) = match self.direction {
-            Up => (0, 1),
-            Right => (1, 0),
-            Down => (0, 1),
-            Left => (1, 0),
-        };
-
-        let offset = Position::from_numeric(x_offset, y_offset).unwrap();
-
-        self.position = self.position.checked_add(offset).context("could not step into darkness, the position is invalid")?;
+        self.position = match self.direction {
+            Up => self.position.checked_decrement_y(1),
+            Right => self.position.checked_increment_x(1),
+            Down => self.position.checked_increment_y(1),
+            Left => self.position.checked_decrement_x(1),
+        }.context("could not step into darkness, the position is invalid")?;
 
         Ok(())
     }
