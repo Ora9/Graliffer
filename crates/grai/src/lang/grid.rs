0cfe5fc5b2751e9f95c5389814219a54cca64dc5
Ora9 <ora9@duck.com>
2026-06-04 17:34:46 +0200

    grai: impl grid

--- crates/grai/src/lang/grid.rs
+++ crates/grai/src/lang/grid.rs
@@ -10,3 +10,28 @@ mod direction;
 pub use direction::*;
 
 pub struct Grid(HashMap<Position, Cell>);
+
+impl Grid {
+    /// Obtain a new empty `Grid`
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    /// Insert given [`Cell`] at [`Position`]
+    pub fn set(&mut self, position: Position, cell: Cell) {
+        if cell.is_empty() {
+            self.0.remove(&position);
+        } else {
+            self.0.insert(position, cell);
+        }
+    }
+
+    /// Get the [`Cell`] at the given [`Position`]
+    pub fn get(&self, position: Position) -> Cell {
+        if let Some(cell) = self.0.get(&position) {
+            cell.clone()
+        } else {
+            Cell::default()
+        }
+    }
+}
