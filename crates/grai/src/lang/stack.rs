ab4f94765a5085f2457593c16f92f60052aeb4b7
Ora9 <ora9@duck.com>
2026-06-05 20:35:27 +0200

    grai: stack impl

--- crates/grai/src/lang/stack.rs
+++ crates/grai/src/lang/stack.rs
@@ -1,3 +1,34 @@
 use crate::Operand;
 
+#[derive(Debug, Default)]
 pub struct Stack(Vec<Operand>);
+
+impl Stack {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn push(&mut self, operand: Operand) {
+        self.0.push(operand);
+    }
+
+    pub fn pop(&mut self) -> Option<Operand> {
+        self.0.pop()
+    }
+
+    // fn pop_err(&mut self) -> Result<Operand, anyhow::Error> {
+    //     self.data.pop().ok_or(anyhow::anyhow!("Could not pop an element from the stack"))
+    // }
+
+    pub fn get_last(&self) -> Option<&Operand> {
+        self.0.last()
+    }
+
+    // pub fn get_last_err(&self) -> Result<&Operand, anyhow::Error> {
+    //     self.data.last().ok_or(anyhow::anyhow!("Could not pop an element from the stack"))
+    // }
+
+    // pub fn iter(&self) -> Iter<'_, Operand> {
+    //     self.0.iter()
+    // }
+}
--- crates/grai/src/lang/word/operand.rs
+++ crates/grai/src/lang/word/operand.rs
@@ -1,9 +1,15 @@
 use crate::{Cell, Position};
 
+#[derive(Debug)]
 pub struct Literal(Cell);
+
+#[derive(Debug)]
 pub struct Address(Position);
+
+#[derive(Debug)]
 pub struct Pointer(Position);
 
+#[derive(Debug)]
 pub enum Operand {
     Literal(Literal),
     Address(Address),
