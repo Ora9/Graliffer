16c013d914859e45c7580e063a644eada58be869
Ora9 <ora9@duck.com>
2026-06-05 23:51:49 +0200

    grai: opcode from cell

--- crates/grai/src/lang/word/opcode.rs
+++ crates/grai/src/lang/word/opcode.rs
@@ -1,6 +1,24 @@
+use std::str::FromStr;
+
+use crate::Cell;
+
+#[derive(Debug, strum_macros::EnumString)]
+#[strum(serialize_all = "lowercase")]
 pub enum Opcode {
     Gou,
     Gor,
     God,
     Gol,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum OpcodeError {
+    #[error("not an opcode, found {0}")]
+    NotAnOpcode(String),
+}
+
+impl Opcode {
+    pub fn from_cell(cell: Cell) -> Result<Opcode, OpcodeError> {
+        Opcode::from_str(&cell.content()).map_err(|_| OpcodeError::NotAnOpcode(cell.content()))
+    }
 }
