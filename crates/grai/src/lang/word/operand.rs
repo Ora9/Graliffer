9550c47f571bb8cb23a2be94a17f55bc07b94688
Ora9 <ora9@duck.com>
2026-06-06 00:39:00 +0200

    grai: impl operand

--- crates/grai/src/lang/word/operand.rs
+++ crates/grai/src/lang/word/operand.rs
@@ -1,17 +1,118 @@
-use crate::{Cell, Position};
+use crate::{Cell, Operand::Address, Position, PositionError};
+
+#[derive(Debug, thiserror::Error)]
+pub enum OperandError {
+    #[error("invalid address, expected to find format `@XY`, found `{0}`")]
+    InvalidAddressFormat(String),
+
+    #[error("invalid address, `{0}`")]
+    InvalidAddress(#[source] PositionError),
+
+    #[error("invalid pointer, expected to find format `&XY`, found `{0}`")]
+    InvalidPointerFormat(String),
+
+    #[error("invalid pointer, `{0}`")]
+    InvalidPointer(#[source] PositionError),
+}
 
 #[derive(Debug)]
 pub struct Literal(Cell);
 
+impl Literal {
+    pub fn new(cell: Cell) -> Self {
+        Self(cell)
+    }
+
+    pub fn from_cell(cell: Cell) -> Self {
+        Self::new(cell)
+    }
+
+    pub fn from_string_trim(string: &str) -> Self {
+        Self::new(Cell::new_trim(string))
+    }
+}
+
 #[derive(Debug)]
 pub struct Address(Position);
 
+impl Address {
+    const PREFIX: char = '@';
+
+    pub fn from_position(position: Position) -> Self {
+        Self(position)
+    }
+
+    pub fn from_cell(cell: Cell) -> Result<Self, OperandError> {
+        let cell_content = cell.content();
+        let pos = cell_content
+            .strip_prefix(Self::PREFIX)
+            .ok_or(OperandError::InvalidAddressFormat(cell.content()))?;
+
+        let pos = Position::from_string(pos).map_err(|err| OperandError::InvalidAddress(err))?;
+
+        Ok(Self::from_position(pos))
+    }
+
+    pub fn as_cell(&self) -> Cell {
+        let (x, y) = self.0.as_textual();
+        Cell::new_trim(&format!("{}{}{}", Self::PREFIX, x, y))
+    }
+}
+
 #[derive(Debug)]
 pub struct Pointer(Position);
+
+impl Pointer {
+    const PREFIX: char = '&';
+
+    pub fn from_position(position: Position) -> Self {
+        Self(position)
+    }
+
+    pub fn from_cell(cell: Cell) -> Result<Self, OperandError> {
+        let cell_content = cell.content();
+        let pos = cell_content
+            .strip_prefix(Self::PREFIX)
+            .ok_or(OperandError::InvalidPointerFormat(cell.content()))?;
+
+        let pos = Position::from_string(pos).map_err(|err| OperandError::InvalidPointer(err))?;
+
+        Ok(Self::from_position(pos))
+    }
+
+    pub fn as_cell(&self) -> Cell {
+        let (x, y) = self.0.as_textual();
+        Cell::new_trim(&format!("{}{}{}", Self::PREFIX, x, y))
+    }
+}
 
 #[derive(Debug)]
 pub enum Operand {
     Literal(Literal),
     Address(Address),
     Pointer(Pointer),
+}
+
+impl Operand {
+    pub fn from_cell(cell: Cell) -> Self {
+        let cell_content = cell.content();
+
+        match cell_content.chars().next() {
+            Some('@') => {
+                if let Ok(address) = Address::from_cell(cell.clone()) {
+                    Self::Address(address)
+                } else {
+                    Self::Literal(Literal::from_cell(cell))
+                }
+            }
+            Some('&') => {
+                if let Ok(pointer) = Pointer::from_cell(cell.clone()) {
+                    Self::Pointer(pointer)
+                } else {
+                    Self::Literal(Literal::from_cell(cell))
+                }
+            }
+            _ => Self::Literal(Literal::from_cell(cell)),
+        }
+    }
 }
