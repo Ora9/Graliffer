9d7ae69cb846ee31806f299b07f55b50d92535a0
Ora9 <ora9@duck.com>
2026-07-10 16:39:16 +0200

    tui: create lib.rs for testing

---
+++ crates/graliffer/src/lib.rs
@@ -0,0 +1,26 @@
+mod app;
+pub use app::*;
+
+mod event;
+pub use event::*;
+
+mod ui;
+pub use ui::*;
+
+mod tui;
+pub use tui::*;
+
+mod input;
+pub use input::*;
+
+mod focus;
+pub use focus::*;
+
+mod context;
+pub use context::*;
+
+mod grid;
+pub use grid::*;
+
+mod console;
+pub use console::*;
