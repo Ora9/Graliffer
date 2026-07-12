606a480977ae203d2f963f7d7b339916b983134b
Ora9 <ora9@duck.com>
2026-07-12 03:00:02 +0200

    tui: keystroke error management

--- crates/graliffer/src/input/keystroke.rs
+++ crates/graliffer/src/input/keystroke.rs
@@ -1,6 +1,9 @@
-use std::fmt::{Display, Formatter};
+use std::{
+    error,
+    fmt::{Display, Formatter},
+};
 
-use crate::{Key, Modifiers};
+use crate::{Key, KeyFromCrosstermError, KeyParseError, Modifiers, ModifiersParseError};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Keystroke {
@@ -17,30 +20,66 @@ impl Keystroke {
     }
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum KeystrokeParseError {
+    #[error("could not parse the key part of the keystroke")]
+    KeyParseError(#[source] KeyParseError),
+
+    #[error("could not parse the modifiers part of the keystroke")]
+    ModifiersParseError(#[source] ModifiersParseError),
+}
+
+impl From<KeyParseError> for KeystrokeParseError {
+    fn from(value: KeyParseError) -> Self {
+        KeystrokeParseError::KeyParseError(value)
+    }
+}
+
+impl From<ModifiersParseError> for KeystrokeParseError {
+    fn from(value: ModifiersParseError) -> Self {
+        KeystrokeParseError::ModifiersParseError(value)
+    }
+}
+
 impl TryFrom<&str> for Keystroke {
-    type Error = eyre::Error;
+    type Error = KeystrokeParseError;
 
     fn try_from(source: &str) -> Result<Self, Self::Error> {
         if let Some((source_modifiers, source_key)) = source.rsplit_once("-") {
             Ok(Self {
-                modifiers: source_modifiers.try_into()?,
-                key: source_key.try_into()?,
+                modifiers: source_modifiers
+                    .try_into()
+                    .map_err(|err| KeystrokeParseError::ModifiersParseError(err))?,
+                key: source_key
+                    .try_into()
+                    .map_err(|err| KeystrokeParseError::KeyParseError(err))?,
             })
         } else {
             Ok(Self {
                 modifiers: Modifiers::NONE,
-                key: source.try_into()?,
+                key: source
+                    .try_into()
+                    .map_err(|err| KeystrokeParseError::KeyParseError(err))?,
             })
         }
     }
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum KeystrokeFromCrosstermError {
+    #[error("invalid crossterm key for keystroke")]
+    KeyFromCrosstermError(#[source] KeyFromCrosstermError),
+}
+
 impl TryFrom<crossterm::event::KeyEvent> for Keystroke {
-    type Error = eyre::Error;
+    type Error = KeystrokeFromCrosstermError;
 
     fn try_from(event: crossterm::event::KeyEvent) -> Result<Self, Self::Error> {
         Ok(Self {
-            key: event.code.try_into()?,
+            key: event
+                .code
+                .try_into()
+                .map_err(|err| KeystrokeFromCrosstermError::KeyFromCrosstermError(err))?,
             modifiers: event.modifiers.into(),
         })
     }
@@ -59,4 +98,12 @@ impl Display for Keystroke {
 
         Ok(())
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display() {}
 }
