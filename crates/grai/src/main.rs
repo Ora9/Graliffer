ca403fdc2a2ae5691ab182812e3543b429a30b73
Ora9 <ora9@duck.com>
2026-07-21 19:52:36 +0200

    grai: a lil cleaner main file

--- crates/grai/src/main.rs
+++ crates/grai/src/main.rs
@@ -1,23 +1,15 @@
-use action::{AnyAction, Timeline};
-use grai::{Cell, Frame, FrameGuard, Grid, GridAction, Head, Position, PositionError, Stack};
+use action::Timeline;
+use grai::{FrameGuard, PositionError};
 
 fn main() -> Result<(), PositionError> {
     let frame_file = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/gri.json"));
 
     let frame = FrameGuard::new(serde_json::from_str(frame_file).expect("frame file invalid"));
 
-    // let frame = FrameGuard::new(Frame {
-    //     grid: Grid::new(),
-    //     head: Head::default(),
-    //     stack: Stack::default(),
-    // });
-
     let mut timeline = Timeline::new(frame.clone());
 
     frame.read(|frame| {
-        let frame_json = serde_json::to_string_pretty(&frame).unwrap();
-
-        println!("{frame_json}");
+        println!("{}", serde_json::to_string_pretty(&frame).unwrap());
     });
 
     Ok(())
