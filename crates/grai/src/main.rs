dea3e9e1da77182bc0bbe8f820b69512c15ccd59
Ora9 <ora9@duck.com>
2026-07-21 19:49:53 +0200

    grai: input file

--- crates/grai/src/main.rs
+++ crates/grai/src/main.rs
@@ -1,26 +1,18 @@
-use std::{any::Any, cell::RefCell, rc::Rc};
-
 use action::{AnyAction, Timeline};
 use grai::{Cell, Frame, FrameGuard, Grid, GridAction, Head, Position, PositionError, Stack};
 
 fn main() -> Result<(), PositionError> {
-    let mut frame = FrameGuard::new(Frame {
-        grid: Grid::new(),
-        head: Head::default(),
-        stack: Stack::default(),
-    });
+    let frame_file = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/gri.json"));
+
+    let frame = FrameGuard::new(serde_json::from_str(frame_file).expect("frame file invalid"));
+
+    // let frame = FrameGuard::new(Frame {
+    //     grid: Grid::new(),
+    //     head: Head::default(),
+    //     stack: Stack::default(),
+    // });
 
     let mut timeline = Timeline::new(frame.clone());
-
-    timeline.act(AnyAction::new(GridAction::Set(
-        Position::from_numeric(5, 8).unwrap(),
-        Cell::new_trim("yey"),
-    )));
-
-    timeline.act(AnyAction::new(GridAction::Set(
-        Position::from_numeric(0, 0).unwrap(),
-        Cell::new_trim("gri"),
-    )));
 
     frame.read(|frame| {
         let frame_json = serde_json::to_string_pretty(&frame).unwrap();
