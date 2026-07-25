a3bd5a8c1cda556e16506fe0dd4f57d47e52d52b
Ora9 <ora9@duck.com>
2026-07-25 11:04:41 +0200

    grai: impl from_cell for word

--- crates/grai/src/lang.rs
+++ crates/grai/src/lang.rs
@@ -4,7 +4,20 @@ pub use opcode::*;
 mod operand;
 pub use operand::*;
 
+use crate::Cell;
+
 pub enum Word {
     Operand(Operand),
     Opcode(Opcode),
+}
+
+impl Word {
+    pub fn from_cell(cell: Cell) -> Self {
+        // avoid clone ?
+        if let Ok(opcode) = Opcode::from_cell(cell.clone()) {
+            Self::Opcode(opcode)
+        } else {
+            Self::Operand(Operand::from_cell(cell))
+        }
+    }
 }
