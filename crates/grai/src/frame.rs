cbe1e535fa8da0a315f195989d252b43c8dcd062
Ora9 <ora9@duck.com>
2026-07-25 11:07:17 +0200

    grai: impl evaluate() for frame, and add Nop as a new opcode

--- crates/grai/src/frame.rs
+++ crates/grai/src/frame.rs
@@ -1,6 +1,6 @@
 use std::{any::type_name_of_val, cell::RefCell, rc::Rc};
 
-use action::{AnyAction, Revert, State};
+use action::{Action, AnyAction, Revert, State};
 use serde::{Deserialize, Serialize};
 
 pub mod examples;
@@ -13,6 +13,8 @@ pub use stack::*;
 
 mod head;
 pub use head::*;
+
+use crate::Word;
 
 #[derive(Debug, thiserror::Error)]
 pub enum FrameError {
@@ -48,7 +50,7 @@ impl State for FrameGuard {
     type Action = AnyAction;
     type Error = FrameError;
 
-    fn act(&mut self, action: &Self::Action) -> Result<Revert, Self::Error> {
+    fn act(&mut self, action: impl Into<Self::Action>) -> Result<Revert, Self::Error> {
         self.write(|frame| frame.act(action))
     }
 }
@@ -60,31 +62,69 @@ pub struct Frame {
     pub stack: Stack,
 }
 
+impl Frame {
+    pub fn step(&mut self) {
+        let cell = self.grid.get(self.head.position);
+
+        if cell.is_empty() {
+            self.act(AnyAction::new(HeadAction::Step));
+        } else {
+            let word = Word::from_cell(cell);
+
+            match word {
+                Word::Opcode(opcode) => {
+                    println!("opcode: {:?}", opcode);
+                    opcode.evaluate(self);
+                }
+                Word::Operand(operand) => {}
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum FrameAction {
+    Step,
+    Run,
+
+    SetBreakpoint(Position),
+    ToggleBreakpoint(Position),
+}
+
+impl Action for FrameAction {}
+
 impl State for Frame {
     type Error = FrameError;
     type Action = AnyAction;
 
-    fn act(&mut self, action: &Self::Action) -> Result<Revert, Self::Error> {
-        if let Some(head_action) = action.downcast_ref::<HeadAction>() {
-            self.head
-                .act(head_action)
-                .map_err(|_| FrameError::HeadError)
-        } else if let Some(stack_action) = action.downcast_ref::<StackAction>() {
-            self.stack
-                .act(stack_action)
-                .map_err(|_| FrameError::StackError)
-        } else if let Some(grid_action) = action.downcast_ref::<GridAction>() {
-            self.grid
-                .act(grid_action)
-                .map_err(|_| FrameError::HeadError)
-        } else {
-            Err(FrameError::UnknownAction(
-                type_name_of_val(action)
-                    .split("::")
-                    .last()
-                    .unwrap_or("unknown action")
-                    .to_string(),
-            ))
-        }
+    fn act(&mut self, action: impl Into<Self::Action>) -> Result<Revert, Self::Error> {
+        Ok(Revert::None)
+
+        // let action = action.into();
+
+        // if let Ok(frame_action) = action.downcast::<FrameAction>() {
+        //     dbg!(frame_action);
+        //     Ok(Revert::None)
+        // } else if let Ok(head_action) = action.downcast::<HeadAction>() {
+        //     self.head
+        //         .act(*head_action)
+        //         .map_err(|_| FrameError::HeadError)
+        // } else if let Ok(stack_action) = action.downcast::<StackAction>() {
+        //     self.stack
+        //         .act(*stack_action)
+        //         .map_err(|_| FrameError::StackError)
+        // } else if let Ok(grid_action) = action.downcast::<GridAction>() {
+        //     self.grid
+        //         .act(*grid_action)
+        //         .map_err(|_| FrameError::HeadError)
+        // } else {
+        //     Err(FrameError::UnknownAction(
+        //         type_name_of_val(&action)
+        //             .split("::")
+        //             .last()
+        //             .unwrap_or("unknown action")
+        //             .to_string(),
+        //     ))
+        // }
     }
 }
--- crates/grai/src/lang/opcode.rs
+++ crates/grai/src/lang/opcode.rs
@@ -1,10 +1,14 @@
 use std::str::FromStr;
 
-use crate::Cell;
+use action::{AnyAction, Revert, State};
+
+use crate::{Cell, Direction, Frame, FrameError, HeadAction};
 
 #[derive(Debug, strum_macros::EnumString)]
 #[strum(serialize_all = "lowercase")]
 pub enum Opcode {
+    Nop,
+
     Gup,
     Gri,
     Gdo,
@@ -20,5 +24,18 @@ pub enum OpcodeError {
 impl Opcode {
     pub fn from_cell(cell: Cell) -> Result<Opcode, OpcodeError> {
         Opcode::from_str(&cell.content()).map_err(|_| OpcodeError::NotAnOpcode(cell.content()))
+    }
+
+    pub fn evaluate(self, frame: &mut Frame) -> Result<Revert, FrameError> {
+        use Opcode::*;
+
+        match self {
+            Nop => Ok(Revert::None),
+
+            Gup => frame.act(AnyAction::new(HeadAction::DirectTo(Direction::Up))),
+            Gri => frame.act(AnyAction::new(HeadAction::DirectTo(Direction::Right))),
+            Gdo => frame.act(AnyAction::new(HeadAction::DirectTo(Direction::Down))),
+            Gle => frame.act(AnyAction::new(HeadAction::DirectTo(Direction::Left))),
+        }
     }
 }
