c1f1d3d2e241030f2d31bf13e96c1ee346de37ff
Ora9 <ora9@duck.com>
2026-07-26 10:48:21 +0200

    grai: Address docs and tests

--- crates/grai/src/lang/operand.rs
+++ crates/grai/src/lang/operand.rs
@@ -99,16 +99,48 @@ impl Display for Literal {
     }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+/// An `Address` contains a [`Position`] and can be used by operations in two ways :
+/// - To designate a certain [`Cell`] in the grid, on wich the operation can act
+///   (e.g the operation `set` takes an address that designate the cell to change)
+/// - To reference another [`Cell`]'s contained [`Literal`] (more frequent use case)
+///   (e.g the operation `add` needs two literal, any one of the two operands can be an address
+///   pointing to a literal, allowing for a level of indirection).
+///
+/// Note that in the second case, any designated cell will *not* be interpreted further, that mean
+/// that if we reference a cell containing another address (or any other special operand) the
+/// operation will use that cell as a literal without any interpretation.
+/// [`Pointer`]s exists just for this purpose, to allow for more that one indirection.
+///
+/// # Format
+/// An address must be in format : `@XY`, with :
+/// - `@` being a prefix (wich denote an address, e.g. a `&` would denote a [`Pointer`])
+/// - `X` and `Y` being respectively the horizontal and vertical axis of a [`Position`] in textual
+/// form, see [position representation](Position#representation) for more informations
+///
+/// Example : `@AB`, `@Q+` or `@8a`
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Address(Position);
 
 impl Address {
     const PREFIX: char = '@';
 
+    /// Get `Self` from a [`Position`]
     pub fn from_position(position: Position) -> Self {
         Self(position)
     }
 
+    /// Return the designated [`Position`]
+    pub fn position(&self) -> &Position {
+        &self.0
+    }
+
+    /// Get `Self` from a [`Cell`] using the `@XY` format (see [address format](Address#Format) for
+    /// more infos)
+    ///
+    /// # Error
+    /// Returns an error if the `Cell` could not be parsed as an address, either because it does not
+    /// start with the right prefix (`@`) or because the following position is invalid,
+    /// see [position representation](Position#representation) for more info
     pub fn from_ref_cell(cell: &Cell) -> Result<Self, OperandError> {
         let pos =
             cell.as_str()
@@ -122,9 +154,17 @@ impl Address {
         Ok(Self::from_position(pos))
     }
 
+    /// Return a [`Cell`], using the `@XY` format, see [address format](Address#format) for more
+    /// information
     pub fn to_cell(&self) -> Cell {
-        let (x, y) = self.0.as_textual();
+        let (x, y) = self.position().as_textual();
         Cell::new_trim(&format!("{}{}{}", Self::PREFIX, x, y))
+    }
+}
+
+impl Display for Address {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.to_cell().as_str())
     }
 }
 
@@ -252,5 +292,48 @@ mod tests {
     fn literal_from_bool() {
         assert_eq!(Literal::from_bool(false).as_str(), "0");
         assert_eq!(Literal::from_bool(true).as_str(), "1");
+    }
+
+    #[test]
+    fn parse_address() -> Result<(), OperandError> {
+        assert_eq!(
+            Address::from_ref_cell(&Cell::new_trim("@yA"))?.position(),
+            &Position::from_string("yA").unwrap()
+        );
+
+        assert_eq!(
+            Address::from_ref_cell(&Cell::new_trim("@/+"))?.position(),
+            &Position::from_string("/+").unwrap()
+        );
+
+        assert_eq!(
+            Address::from_ref_cell(&Cell::new_trim(" @A")),
+            Err(OperandError::InvalidAddressFormat(" @A".to_string()))
+        );
+
+        assert_eq!(
+            Address::from_ref_cell(&Cell::new_trim("AA")),
+            Err(OperandError::InvalidAddressFormat("AA".to_string()))
+        );
+
+        assert_eq!(
+            Address::from_ref_cell(&Cell::new_trim("@p")),
+            Err(OperandError::InvalidAddress(PositionError::WrongFormat(
+                "p".to_string()
+            )))
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn address_to_cell() {
+        assert_eq!(
+            Address::from_position(Position::from_string("a5").unwrap()).to_string(),
+            String::from("@a5")
+        );
+
+        let address = Address::from_position(Position::from_string("oO").unwrap());
+        assert_eq!(address.to_cell().to_string(), address.to_string());
     }
 }
