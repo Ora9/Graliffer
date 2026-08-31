66ed3e34512cf3efe63c9bfbcfaef7be4eebf924
Ora9 <ora9@duck.com>
2026-08-31 15:30:39 +0200

    tui: refactor handle key event

--- crates/graliffer/src/input/handle_events.rs
+++ crates/graliffer/src/input/handle_events.rs
@@ -1,24 +1,42 @@
-use act::State;
+use act::{Revert, State};
 use crossterm::event::{KeyEvent, MouseEvent};
+use log::debug;
 use ratatui::layout::Position;
 
-use crate::{AppState, Context, GridView, Key, Keystroke, PickerView, View};
+use crate::{AppState, Context, GridView, Key, Keystroke, Picker, PickerView, View};
 
 impl AppState {
     pub fn handle_key_events(&mut self, key_event: KeyEvent, app_context: Context) {
-        if let Result::Ok(keystroke) = Keystroke::try_from(key_event) {
+        let revert = if let Ok(keystroke) = Keystroke::try_from(key_event) {
             if let Some(action) = self.keymap.find(app_context, keystroke) {
-                // debug!("{:?}", action);
-                let _ = self.act(action);
+                match self.act(action) {
+                    Ok(revert) => revert,
+                }
             } else if let Key::Char(char) = keystroke.key {
-                let action = match self.focused().to_string().as_str() {
-                    "Grid" => GridView::input_sink_action(char.to_string()),
-                    "Picker" => PickerView::input_sink_action(char.to_string()),
-                    _ => None,
-                };
-
-                let _ = self.act(action.unwrap());
+                self.handle_input_sink(char.to_string())
+            } else {
+                Revert::None
             }
+        } else {
+            Revert::None
+        };
+
+        debug!("{:?}", revert);
+    }
+
+    pub fn handle_input_sink(&mut self, input: String) -> Revert {
+        let action = match self.focused().to_string().as_str() {
+            "Grid" => GridView::input_sink_action(input),
+            "Picker" => PickerView::input_sink_action(input),
+            _ => None,
+        };
+
+        if let Some(action) = action {
+            match self.act(action) {
+                Ok(revert) => revert,
+            }
+        } else {
+            Revert::None
         }
     }
 
