42670d4d2a61a4f28f8992316090f3be2bd9a3db
Ora9 <ora9@duck.com>
2026-09-04 14:59:55 +0200

    act: update tests accordingly

--- crates/act/tests/common/pond.rs
+++ crates/act/tests/common/pond.rs
@@ -1,8 +1,8 @@
 use std::{collections::HashMap, convert::Infallible, error::Error, fmt::Display};
 
-use act::{Action, Revert, State};
+use act::{Action, ConvertState, Revert, State};
 
-use crate::common::pond::PondAction::BeginPatPatingFrog;
+// use crate::common::pond::PondAction::BeginPatPatingFrog;
 
 #[derive(Debug)]
 pub struct Camp {
@@ -22,7 +22,7 @@ impl State for Camp {
     type Action = CampAction;
     type Error = Infallible;
 
-    fn act(&mut self, action: impl Into<Self::Action>) -> Result<Revert, Self::Error> {
+    fn act(&mut self, action: impl Into<Self::Action>) -> Result<Revert<Self>, Self::Error> {
         match action.into() {
             CampAction::LitFire(state) => {
                 let revert = Revert::new(CampAction::LitFire(self.fire));
@@ -79,17 +79,23 @@ pub enum PondAction {
     StopPatPatingFrog(FrogName), // But why tho ??
 }
 
+impl From<CampAction> for PondAction {
+    fn from(value: CampAction) -> Self {
+        PondAction::CampAction(value)
+    }
+}
+
 impl Action for PondAction {}
 
 impl State for Pond {
     type Action = PondAction;
     type Error = PondError;
 
-    fn act(&mut self, action: impl Into<Self::Action>) -> Result<Revert, Self::Error> {
+    fn act(&mut self, action: impl Into<Self::Action>) -> Result<Revert<Self>, Self::Error> {
         match action.into() {
-            PondAction::CampAction(camp_action) => {
-                self.camp.act(camp_action).map_err(|_| unreachable!())
-            }
+            PondAction::CampAction(camp_action) => match self.camp.act(camp_action) {
+                Ok(revert) => Ok(ConvertState::convert_state(revert)),
+            },
             PondAction::IntroduceFrog(frog_name) => {
                 self.frogs.insert(frog_name.clone(), Frog::default());
                 Ok(Revert::new(PondAction::SendFrogToATrip(frog_name)))
@@ -109,7 +115,7 @@ impl State for Pond {
             PondAction::StopPatPatingFrog(frog_name) => {
                 if let Some(frog) = self.frogs.get_mut(&frog_name) {
                     frog.happy = false;
-                    Ok(Revert::new(BeginPatPatingFrog(frog_name)))
+                    Ok(Revert::new(PondAction::BeginPatPatingFrog(frog_name)))
                 } else {
                     Err(PondError::NoFrogThisName(frog_name))
                 }
